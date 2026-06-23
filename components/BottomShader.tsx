05a70d9ad9f13dc1c5105794fdce7a43157afff9
zAcherttp <saladegg24@gmail.com>
2026-06-23 21:53:52 +0700

    style: increase waveSpeed to 0.15 in BottomShader

--- components/BottomShader.tsx
+++ components/BottomShader.tsx
@@ -11,7 +11,7 @@ export default function BottomShader() {
   return (
     <div className="absolute left-0 right-0 bottom-0 h-[120px] overflow-hidden pointer-events-none z-0 opacity-60">
       <Dither
-        waveSpeed={0.02}
+        waveSpeed={0.15}
         waveFrequency={2.5}
         waveAmplitude={0.25}
         waveColor={[0.92, 0.88, 0.98]} // Soft pastel lavender
