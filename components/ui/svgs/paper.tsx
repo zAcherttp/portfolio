002778b8cddee0926003a9239dc75515add930aa
zAcherttp <saladegg24@gmail.com>
2026-06-23 15:23:45 +0700

    feat(svgs): add brand svg components and generate index exports

---
+++ components/ui/svgs/paper.tsx
@@ -0,0 +1,9 @@
+import type { SVGProps } from "react";
+
+const Paper = (props: SVGProps<SVGSVGElement>) => (
+  <svg {...props} viewBox="0 0 39 39" fill="none">
+    <path d="M39 24H24V6H6V24H24V39H0V6H6V0H39V24Z" fill="#81ADEC" />
+  </svg>
+);
+
+export { Paper };
